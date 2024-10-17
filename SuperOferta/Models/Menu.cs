6f99e25012770502e89c9eaa7b4dcb1c7ac16cd2
MatiasCionci <cioncimatias2@gmail.com>
2024-10-17 00:46:22 -0300

    Menu Actualizado

---
+++ SuperOferta/Models/Menu.cs
@@ -0,0 +1,13 @@
+﻿namespace SuperOferta.Models
+{
+    public class Menu
+    {
+        public string? Titulo1 { get; set; }
+        public string? Titulo2 { get; set; }
+        
+        public Menu(String titulo,string second)
+        {
+            Titulo1 = titulo; Titulo2=second;
+        }
+    }
+}

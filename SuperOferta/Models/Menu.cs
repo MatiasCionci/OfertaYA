44c1576b2796221a65e63bae21fc8022870595d3
MatiasCionci <cioncimatias2@gmail.com>
2024-10-22 02:31:00 -0300

    Actualizacion Menu

--- SuperOferta/Models/Menu.cs
+++ SuperOferta/Models/Menu.cs
@@ -4,10 +4,13 @@
     {
         public string? Titulo1 { get; set; }
         public string? Titulo2 { get; set; }
-        
-        public Menu(String titulo,string second)
+
+        public string? Titulo6 { get; set; }
+
+        public Menu(String titulo,string second, string? titulo6)
         {
-            Titulo1 = titulo; Titulo2=second;
+            Titulo1 = titulo; Titulo2 = second;
+            Titulo6 = titulo6;
         }
     }
 }
